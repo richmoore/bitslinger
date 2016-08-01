9660f8d96a346910ca04ecff7039018a3558863b
Richard J. Moore <rich@kde.org>
2016-08-01 17:55:40 +0100

    More work on the journal

--- journal.cpp
+++ journal.cpp
@@ -1,3 +1,5 @@
+#include <QDateTime>
+
 #include "journal.h"
 
 Journal::Journal(QObject *parent) : QObject(parent)
@@ -8,6 +10,7 @@ Journal::Journal(QObject *parent) : QObject(parent)
 void Journal::recordEvent(int id, Connection::EventType type, Connection::Direction dir, Connection::State newState, const QByteArray &content)
 {
     JournalEntry *entry = new JournalEntry;
+    entry->timestamp = QDateTime::currentMSecsSinceEpoch();
     entry->connectionId = id;
     entry->type = type;
     entry->dir = dir;
--- journal.h
+++ journal.h
@@ -3,16 +3,21 @@
 
 #include <QObject>
 #include <QList>
+#include <QColor>
 
 #include "connection.h"
 
+//### We need to be able to lookup the host etc. too
 struct JournalEntry
 {
+    qint64 timestamp;
     int connectionId;
     Connection::EventType type;
     Connection::Direction dir;
     Connection::State newState;
     QByteArray content;
+    QColor color;
+    QString comment;
 };
 
 class Journal : public QObject
@@ -24,6 +29,7 @@ public:
 signals:
 
 public slots:
+    //### Change this to a direct function call?
     void recordEvent(int id, Connection::EventType type, Connection::Direction dir, Connection::State newState, const QByteArray &content);
 
 public:
